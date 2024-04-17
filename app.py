292cad1e84a2abcfe45e826960d398b919143bf9
YiChao Ding <jeffding890430@163.com>
2024-04-17 10:26:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
+import torch
 import os
-os.system("lmdeploy serve gradio internlm/internlm-chat-7b")
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+
+base_path = './internlm2lianghua'
+# download repo to the base_path directory using git
+os.system('apt install git')
+os.system('apt install git-lfs')
+os.system(f'git clone https://code.openxlab.org.cn/JeffDing/internlm2lianghua.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
+
+os.system(f'lmdeploy serve gradio {base_path}')
