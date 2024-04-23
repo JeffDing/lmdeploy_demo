72b3ea4c92a9aebe13409ec2cf356371a5fb1aab
YiChao Ding <jeffding890430@163.com>
2024-04-23 10:57:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
+import os
+import gradio as gr
+from lmdeploy import pipeline, TurbomindEngineConfig,GenerationConfig
+
+backend_config = TurbomindEngineConfig(cache_max_entry_count=0.01,model_format="awq")
+
+# download internlm2 to the base_path directory using git tool
+base_path = './internlm2lianghua'
+os.system(f'git clone https://code.openxlab.org.cn/JeffDing/internlm2lianghua.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
+
+pipe = pipeline(base_path,backend_config=backend_config)
+gen_config = GenerationConfig(top_p=0.8,top_k=40,temperature=0.8,max_new_tokens=1024)
+
+def chat(message,history):
+  response = pipe(message, gen_config = gen_config)
+return response.text
+
+demo = gr.ChatInterface(
+  fn = chat,
+  title="InternLM2-Chat-1.8_4bit-self-cognition",
+  description="""InternLM is mainly developed by Shanghai AI Laboratory. """,
+)
+demo.queue(1).launch()
+
+
+'''
 import torch
 import os
 from lmdeploy.serve.gradio.turbomind_coupled import run_local
 from lmdeploy.messages import TurbomindEngineConfig
 
-base_path = './internlm2lianghua'
+base_path = 
 # download repo to the base_path directory using git
-os.system(f'git clone https://code.openxlab.org.cn/JeffDing/internlm2lianghua.git {base_path}')
-os.system(f'cd {base_path} && git lfs pull')
+
 
 backend_config = TurbomindEngineConfig(max_batch_size=8)
 model_path = base_path
 model_name = 'internlm2lianghua'
 run_local(model_path, model_name, backend_config=backend_config, server_name="huggingface-space",max_threads=1)
+'''
