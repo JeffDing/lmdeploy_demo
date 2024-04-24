8b9167c0acbbf4aa25e861322e514d9990a1a67a
YiChao Ding <jeffding890430@163.com>
2024-04-24 09:37:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from lmdeploy import pipeline, TurbomindEngineConfig,GenerationConfig
 backend_config = TurbomindEngineConfig(cache_max_entry_count=0.01,model_format="awq")
 
 # download internlm2 to the base_path directory using git tool
-base_path = './internlm2-chat-1.8b'
+base_path = './internlm2-chat-1_8b'
 os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-1.8b.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
